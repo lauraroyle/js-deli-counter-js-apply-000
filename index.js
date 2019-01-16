1ab19098317cb2fc448e1ff3c9b9fec8d49eed1d
Laura Royle <lauraamie.royle@gmail.com>
2019-01-16 15:40:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,18 +22,14 @@ function nowServing (katzDeli) {
 // return the string including the variable (eg nextPerson)
 // used else statement so if array is 0, the string is returned to show noone in line
 
-var array = [];
-function currentLine(katzDeli) {
-  if (katzDeli.length == 0) {
+var array = []; // this is needed later to house the position and person in the line
+function currentLine(katzDeli) { 
+  if (katzDeli.length == 0) {  // if the line length is equal to zero, then return the line is empty
     return `The line is currently empty.`;
   } else {
-    for (var i = 0; i < katzDeli.length; i++) {
-      array.push(` ${i+1}. ${katzDeli[i]}`);
+    for (var i = 0; i < katzDeli.length; i++) { // if the line is less than zero then keep incrementally adding to the line
+      array.push(` ${i+1}. ${katzDeli[i]}`); // array.push is taking i+1 =1 (to account for array index starting from 0 and putting this infront of the current line (katzDeli)
     }
-    return `The line is currently:` + array;
+    return `The line is currently:` + array; // string plus the array which (as above) includes the person and the position in the queue
   }
 }
-
-// function currentLine accepts 'katzDeli' (ie. the current line) as a parameter
-// if the line length is equal to zero, then return the line is empty
-// then used a 'for' loop
